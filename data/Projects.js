9bd16f290d615b1c2395f46b5b637845b2363a9e
painzy740 <muhammadrafif0110@gmail.com>
2025-03-30 18:29:03 +0700

    Add files via upload

---
+++ data/Projects.js
@@ -0,0 +1,62 @@
+export const Projects = [
+  {
+    id: 1,
+    name: 'Website Gabut',
+    slug: 'sgcid',
+    img: 'sgcid',
+    demo: 'https://painzysite.biz.id',
+    sourceCode: 'https://painzysite.biz.id',
+    description:
+      'Sebuah Website Gabut Yang berisi chat ai !!',
+  },
+  {
+    id: 3,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  },
+  {
+    id: 3,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  },
+  {
+    id: 4,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  },
+  {
+    id: 5,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  },
+  {
+    id: 6,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: '#',
+    sourceCode: '#',
+    description:
+      'Nantikan project-project mendatang lainnya.',
+  },
+];
